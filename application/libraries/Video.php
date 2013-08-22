107730c78e4420b85781549384764feb852ccf13
alemohamad <alejandro.mohamad@gmail.com>
2013-08-22 13:58:10 -0300

    Library and documentation added.

---
+++ application/libraries/Video.php
@@ -0,0 +1,288 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+/**
+ * Video Library
+ *
+ * Library that get information from YouTube and Vimeo videos.
+ *
+ * @package		CodeIgniter
+ * @category	Library
+ * @author		Ale Mohamad
+ * @link		alemohamad.com
+ * @version 	1.0
+ */
+
+class Video {
+
+    private $_video_id = FALSE;
+    private $_video_type = FALSE;
+
+    /**
+     * Add Video
+     *
+     * This method automatically recognizes if the video is from YouTube or Vimeo.
+     *
+     * @access	public
+     * @param	string	video url
+     * @return	string	video ID
+     */
+    public function add_video($link)
+    {
+        if(!preg_match("/vimeo/", $link)) {
+            $vendor = 'youtube';
+        } else {
+            $vendor = 'vimeo';
+        }
+
+        if($vendor == 'youtube') {
+            if($id = $this->_get_youtube_id($link)) {
+                $this->_video_id = $id;
+                $this->_video_type = 'youtube';
+                return $this->_video_id;
+            } else {
+                return FALSE;
+            }
+        } else if($vendor == 'vimeo') {
+            if($id = $this->_get_vimeo_id($link)) {
+                $this->_video_id = $id;
+                $this->_video_type = 'vimeo';
+                return $this->_video_id;
+            } else {
+                return FALSE;
+            }
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Return Video ID
+     *
+     * @access	public
+     * @return	string	video ID
+     */
+    public function get_video_id() {
+        return $this->_video_id;
+    }
+
+    /**
+     * Return Video Type (youtube or vimeo)
+     *
+     * @access	public
+     * @return	string	video type
+     */
+    public function get_video_type() {
+        return $this->_video_type;
+    }
+
+    /**
+     * Return url of video
+     *
+     * @access	public
+     * @return	string	video url
+     */
+    public function get_video_url()
+    {
+        if(!$this->_verifyValidID()) { return FALSE; }
+
+        $id = $this->get_video_id();
+
+        if($this->_video_type == 'youtube') {
+            return 'https://www.youtube.com/watch?v=' . $id;
+        } else if($this->_video_type == 'vimeo') {
+            return 'https://vimeo.com/' . $id;
+        }
+    }
+
+    /**
+     * Return Video HTML embed code
+     *
+     * @access	public
+     * @param	number	width
+     * @param	number	height
+     * @return	string	html code
+     */
+    public function get_embed_video($width = 500, $height = 280)
+    {
+        if(!$this->_verifyValidID()) { return FALSE; }
+
+        $id = $this->get_video_id();
+
+        if($this->_video_type == 'youtube') {
+            return '<iframe width="' . $width . '" height="' . $height . '" src="http://www.youtube.com/embed/' . $id . '/" frameborder="0" allowfullscreen></iframe>';
+        } else if($this->_video_type == 'vimeo') {
+            return '<iframe src="http://player.vimeo.com/video/' . $id . '?color=ffffff" width="' . $width . '" height="' . $height . '" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+        }
+    }
+
+    /**
+     * Return Video thumbs
+     *
+     * @access	public
+     * @return	array	thumbs
+     */
+    public function get_thumbs()
+    {
+        if(!$this->_verifyValidID()) { return FALSE; }
+
+        $id = $this->get_video_id();
+
+        if($this->_video_type == 'youtube') {
+            return array(
+                'default' => 'http://img.youtube.com/vi/' . $id . '/default.jpg',
+                '0' => 'http://img.youtube.com/vi/' . $id . '/0.jpg',
+                '1' => 'http://img.youtube.com/vi/' . $id . '/1.jpg',
+                '2' => 'http://img.youtube.com/vi/' . $id . '/2.jpg',
+                '3' => 'http://img.youtube.com/vi/' . $id . '/3.jpg'
+            );
+        } else if($this->_video_type == 'vimeo') {
+            $hash = json_decode(file_get_contents('http://vimeo.com/api/v2/video/' . $id . '.json'));
+
+            return array(
+                '0' => $hash[0]->thumbnail_small,
+                '1' => $hash[0]->thumbnail_medium,
+                '2' => $hash[0]->thumbnail_large,
+                '3' => str_replace("_640.jpg", "_1280.jpg", $hash[0]->thumbnail_large)
+            );
+        }
+    }
+
+    /**
+     * Return Video info
+     *
+     * @access	public
+     * @return	array	info
+     */
+    public function get_video_info()
+    {
+        if(!$this->_verifyValidID()) { return FALSE; }
+
+        $id = $this->get_video_id();
+
+        if($this->_video_type == 'youtube') {
+            $video_info = json_decode(file_get_contents('https://gdata.youtube.com/feeds/api/videos/' . $id . '?v=2&alt=json'));
+            $info = $video_info->entry;
+
+            return array(
+                'title' => $info->{'media$group'}->{'media$title'}->{'$t'},
+                'description' => $info->{'media$group'}->{'media$description'}->{'$t'},
+                'author' => $info->{'media$group'}->{'media$credit'}[0]->{'yt$display'},
+                'mobile_url' => 'http://m.youtube.com/watch?v=' . $id,
+                'short_url' => 'http://youtu.be/' . $id,
+                'category' => $info->{'media$group'}->{'media$category'}[0]->label,
+                'duration' => $info->{'media$group'}->{'yt$duration'}->seconds,
+                'likes' => $info->{'yt$rating'}->numLikes,
+                'dislikes' => $info->{'yt$rating'}->numDislikes,
+                'favoriteCount' => $info->{'yt$statistics'}->favoriteCount,
+                'viewCount' => $info->{'yt$statistics'}->viewCount,
+                'commentsCount' => $info->{'gd$comments'}->{'gd$feedLink'}->countHint,
+                'rating' => $info->{'gd$rating'}->average
+            );
+        } else if($this->_video_type == 'vimeo') {
+            $info = json_decode(file_get_contents('http://vimeo.com/api/v2/video/' . $id . '.json'));
+
+            return array(
+                'title' => $info[0]->title,
+                'description' => $info[0]->description,
+                'user_name' => $info[0]->user_name,
+                'user_url' => $info[0]->user_url,
+                'mobile_url' => $info[0]->mobile_url,
+                'stats_number_of_likes' => $info[0]->stats_number_of_likes,
+                'stats_number_of_plays' => $info[0]->stats_number_of_plays,
+                'stats_number_of_comments' => $info[0]->stats_number_of_comments,
+                'duration' => $info[0]->duration,
+                'tags' => $info[0]->tags
+            );
+        }
+    }
+
+    /**
+     * Get YouTube ID
+     *
+     * @access	private
+     * @param	string	url
+     * @return	string	Video ID
+     */
+    private function _get_youtube_id( $url = '')
+    {
+        if ( $url === '' ) { return FALSE; }
+        if (!$this->_isValidURL( $url )) { return FALSE; }
+
+        preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $url, $matches);
+        if(!$matches) { return FALSE; }
+
+        if ( !$this->_isValidID( $matches[0] )) {
+            return FALSE;
+        } else{
+            return $matches[0];
+        }
+    }
+
+    /**
+     * Get Vimeo ID
+     *
+     * @access	private
+     * @param	string	url
+     * @return	string	Video ID
+     */
+    private function _get_vimeo_id( $url = '')
+    {
+        if ( $url === '' ) { return FALSE; }
+        if ($this->_isValidURL( $url )) {
+            sscanf(parse_url($url, PHP_URL_PATH), '/%d', $vimeo_id);
+        } else {
+            $vimeo_id = $url;
+        }
+
+        return ($this->_isValidID($vimeo_id,TRUE)) ? $vimeo_id : FALSE;
+    }
+
+    /**
+     * Verifies if the Video URL is valid
+     *
+     * @access	private
+     * @param	string	url
+     * @return	boolean
+     */
+    private function _isValidURL($url = '')
+    {
+        return preg_match('/^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:.[A-Z0-9][A-Z0-9_-]*)+):?(d+)?/i', $url);
+    }
+
+    /**
+     * Verifies if the Video URL is valid
+     *
+     * @access	private
+     * @param	string	id
+     * @param	boolean	vimeo
+     * @return	boolean
+     */
+    private function _isValidID($id = '', $vimeo=FALSE)
+    {
+        if ($vimeo)
+            $headers = get_headers('http://vimeo.com/' . $id);
+        else
+            $headers = get_headers('http://gdata.youtube.com/feeds/api/videos/' . $id);
+
+        if (!strpos($headers[0], '200')) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    /**
+     * Verifies if the saved Video ID is valid
+     *
+     * @access	private
+     * @return	string	id
+     */
+    private function _verifyValidID() {
+        $type = ($this->_video_type == 'vimeo') ? TRUE : FALSE;
+        $id = ($this->_isValidID($this->get_video_id(), $type)) ? $this->get_video_id() : FALSE;
+        return $id;
+    }
+
+}
+
+/* End of file Video.php */
---
+++ readme.md
@@ -0,0 +1,98 @@
+# CodeIgniter Library: Internet video (YouTube + Vimeo)
+
+**ci-video**
+
+## About this library
+
+This CodeIgniter's Library is used to get information from YouTube and Vimeo videos.
+
+Its usage is recommended for CodeIgniter 2 or greater.
+
+## Usage
+
+```php
+$this->load->library('Video');
+
+$this->video->add_video('VIDEO_URL');
+
+/**
+ * Some examples:
+ * 
+ * http://www.youtube.com/watch?v=JRfuAukYTKg
+ * http://youtu.be/JRfuAukYTKg
+ * https://vimeo.com/34035823
+ **/
+
+$video_id = $this->video->get_video_id();
+
+$video_type = $this->video->get_video_type(); // youtube or vimeo
+
+$video_url = $this->video->get_video_url();
+
+$video_embed = $this->video->get_embed_video();
+
+// you can also define the width and height of the embed html code
+$video_embed = $this->video->get_embed_video(500, 280);
+
+$video_thumbs = $this->video->get_thumbs(); // Array
+
+$video_info = $this->video->get_video_info(); // Array
+```
+
+The thumbs array vary from the vendor:
+
+```
+// YouTube Thumbs
+array(5) {
+  ["default"] => string(49) "http://img.youtube.com/vi/JRfuAukYTKg/default.jpg"
+  [0] => string(43) "http://img.youtube.com/vi/JRfuAukYTKg/0.jpg"
+  [1] => string(43) "http://img.youtube.com/vi/JRfuAukYTKg/1.jpg"
+  [2] => string(43) "http://img.youtube.com/vi/JRfuAukYTKg/2.jpg"
+  [3] => string(43) "http://img.youtube.com/vi/JRfuAukYTKg/3.jpg"
+}
+
+// Vimeo Thumbs
+array(4) {
+  [0] => string(50) "http://b.vimeocdn.com/ts/431/559/431559530_100.jpg"
+  [1] => string(50) "http://b.vimeocdn.com/ts/431/559/431559530_200.jpg"
+  [2] => string(50) "http://b.vimeocdn.com/ts/431/559/431559530_640.jpg"
+  [3] => string(51) "http://b.vimeocdn.com/ts/431/559/431559530_1280.jpg"
+}
+```
+
+The info array vary from the vendor:
+
+```
+// YouTube Info
+array(13) {
+  ["title"] => string(31) "David Guetta - Titanium ft. Sia"
+  ["description"] => string(643) "From the album Nothing But The Beat Ultimate (...)"
+  ["author"] => string(15) "davidguettavevo"
+  ["mobile_url"] => string(40) "http://m.youtube.com/watch?v=JRfuAukYTKg"
+  ["short_url"] => string(27) "http://youtu.be/JRfuAukYTKg"
+  ["category"] => string(5) "Music"
+  ["duration"] => string(3) "246"
+  ["likes"] => string(6) "793989"
+  ["dislikes"] => string(5) "18993"
+  ["favoriteCount"] => string(1) "0"
+  ["viewCount"] => string(9) "206439645"
+  ["commentsCount"] => int(173222)
+  ["rating"] => float(4.9065514)
+}
+
+// Vimeo Info
+array(10) {
+  ["title"] => string(33) "David Guetta Feat. Sia - Titanium"
+  ["description"] => string(669) "Music by David Guetta feat. Sia (...)"
+  ["user_name"] => string(12) "David Wilson"
+  ["user_url"] => string(34) "http://vimeo.com/thisisdavidwilson"
+  ["mobile_url"] => string(27) "http://vimeo.com/m/34035823"
+  ["stats_number_of_likes"] => int(882)
+  ["stats_number_of_plays"] => int(109747)
+  ["stats_number_of_comments"] => int(48)
+  ["duration"] => int(246)
+  ["tags"] => string(87) "David Guetta, Titanium, Sia, David Wilson, Larkin Seiple, Iconoclast, Ryan Lee, Super 8"
+}
+```
+
+![Ale Mohamad](http://alemohamad.com/github/logo2012am.png)
